2892ba4d600c1d55e49b602d10aacd09b5718a22
uXmuu <78486414+uXmuu@users.noreply.github.com>
2022-01-23 17:49:56 +0000

    Update uxfetch.c

--- uxfetch.c
+++ uxfetch.c
@@ -5,14 +5,8 @@
 #include<sys/utsname.h>
 #include<ctype.h>
 
-
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
 #define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
 #define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
 #define RESET "\x1B[0m"
 
 int getInt(char **str){
@@ -38,37 +32,49 @@ int getMem(){
 	int memAvailable = getInt(mem3);
 	int memUsed = totalMem-=memAvailable;
 	memUsed = memUsed/1124;
-	//char m[] = ("%d/%d",memUsed,totalMem);
 	fclose(meminfo);
 	return memUsed;
 }
-int main()
+int getTotalMem(){
+	FILE* meminfo = fopen("/proc/meminfo","r");
+	char* mem[1000];
+	fgets(mem, 1000, meminfo);
+	int totalMem = getInt(mem);
+	totalMem = totalMem/1024;
+	fclose(meminfo);
+	return totalMem;
+}
+int main(int argc, char *argv[])
 {
-	struct utsname buf1;	
-	char *test;
+	struct utsname buf1;
 	errno = 0;
 	if(uname(&buf1)!=0){
 	
 		exit(EXIT_FAILURE);
 	}
+	int totalMem = getTotalMem();
 	int memUsed = getMem();
+
+	printf(YEL "      _____\n    \\-     -/\n \\_/         \\ \thostname %s\t%s\n %s|        %sO O%s |\tVersion  %s\t%s\n %s|_  <   )  3 )\tkernel  %s\t%s\n%s / \\         /\tmemory%s\t\t%dM / %dM\n    %s/-_____-\\\n",RESET,buf1.nodename,YEL,CYN,YEL,RESET,buf1.version,YEL,RESET,buf1.release,YEL,RESET,memUsed,totalMem,YEL);
+
+	/*
 	printf(YEL "      _____\n");
 	printf(YEL "    \\-     -/\n");
 	printf(YEL " \\_/         \\ \thostname  %s\t%s\n",RESET, buf1.nodename);
 	printf(YEL " |        %sO O%s |\tVersion  %s\t%s\n",CYN,YEL,RESET, buf1.version);
 	printf(YEL " |_  <   )  3 )\tkernel  %s\t%s\n",RESET, buf1.release);
-	printf(YEL " / \\         /\tmemory%s\t\t%dM\n",RESET,memUsed);
-	printf(test);
-	free(test);
+	printf(YEL " / \\         /\tmemory%s\t\t%dM / %dM\n",RESET,memUsed,totalMem);
 	printf(YEL "    /-_____-\\\n");
-	
+	*/
 }
 
-/*	  _____
+/*
+      _____
     \-     -/
  \_/         \
  |        O O |
  |_  <   )  3 )
  / \         /
     /-_____-\
+
 */
